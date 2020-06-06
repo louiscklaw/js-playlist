c0f3cdef9d4fa48a813a56ed72b3f6b2f900ac5b
louiscklaw <louiscklaw@gmail.com>
2020-06-06 15:08:15 +0800

    update template,

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-touch index.html
+set -ex
 
-mkdir css js
+TEST=`echo $PWD|rev |cut -d'/' -f1 |rev`
+
+# git checkout -b test/$TEST
+
+mkdir -p css js
+
+cp ../scripts/template.html index.html
+cp ../scripts/template.style.css css/style.css
+cp ../scripts/template.app.js js/app.js
+
+git add .
+git commit -m"init commit $TEST"
