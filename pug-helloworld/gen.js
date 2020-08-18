679dbf87c309219b74a0f89d4104afe15106690c
louiscklaw <louiscklaw@gmail.com>
2020-08-18 13:39:29 +0800

    update,

---
+++ pug-helloworld/gen.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const pug = require('pug');
+
+// Compile the source code
+const compiledFunction = pug.compileFile('template.pug');
+
+const OUTPUT_PATH='public'
+
+fs.writeFileSync(`${OUTPUT_PATH}/index.html`,compiledFunction({
+  name: 'Timothy'
+}))
