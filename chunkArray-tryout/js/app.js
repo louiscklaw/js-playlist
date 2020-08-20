7e198901c9ba45a595ce273e0e86ce61cd6e4830
louiscklaw <louiscklaw@gmail.com>
2020-08-21 00:10:41 +0800

    update,

--- chunkArray-tryout/js/app.js
+++ chunkArray-tryout/js/app.js
@@ -1,1 +1,17 @@
-console.log("helloworld")
+function chunkArray(myArray, chunk_size){
+  var index = 0;
+  var arrayLength = myArray.length;
+  var tempArray = [];
+
+  for (index = 0; index < arrayLength; index += chunk_size) {
+      myChunk = myArray.slice(index, index+chunk_size);
+      // Do something if you want with the group
+      tempArray.push(myChunk);
+  }
+
+  return tempArray;
+}
+
+var result = chunkArray([1,2,3,4,5,6], 4);
+
+console.log(result);
