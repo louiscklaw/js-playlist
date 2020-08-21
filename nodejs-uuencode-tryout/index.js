a470ad02918c306f4639695061381756abe61dce
louiscklaw <louiscklaw@gmail.com>
2020-08-21 19:24:45 +0800

    update,

---
+++ nodejs-uuencode-tryout/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+var uuencode = require( 'uuencode' );
+
+// encode something
+var encoded = uuencode.encode('Unix-to-Unix encoding');
+// '556YI>"UT;RU5;FEX(&5N8V]D:6YG\n'
+
+// decode something
+var decoded = uuencode.decode('556YI>"UT;RU5;FEX(&5N8V]D:6YG\n');
+// 'Unix-to-Unix encoding'
+
+console.log(encoded)
