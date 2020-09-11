7f89363ede8ab5b764de2a8e946dd53f3c0f637d
louiscklaw <louiscklaw@gmail.com>
2020-09-11 20:22:53 +0800

    ci: update to add pngjs to github actions,

--- pixelmatch-tryout/index.js
+++ pixelmatch-tryout/index.js
@@ -1,1 +1,12 @@
-console.log('helloworld')
+const fs = require('fs');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
+
+const img1 = PNG.sync.read(fs.readFileSync('helloworld-1.png'));
+const img2 = PNG.sync.read(fs.readFileSync('helloworld-2.png'));
+const {width, height} = img1;
+const diff = new PNG({width, height});
+
+pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
+
+fs.writeFileSync('diff.png', PNG.sync.write(diff));
