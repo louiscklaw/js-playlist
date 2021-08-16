5f92643c4ec8b750ee815fb026399ce43108b460
louiscklaw <louiscklaw@gmail.com>
2021-08-16 20:03:35 +0800

    update,

---
+++ nightmare-helloworld-tryout/helloworld-tryout/init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+npm init -y
+
+npm install --save nightmare
---
+++ nightmare-helloworld-tryout/mocha-helloworld-tryout/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+npm init -y
+
+npm install --save nightmare
+npm install --save chai
+npm install --save mocha
