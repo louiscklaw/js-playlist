a0070456c01f0d566c965caf4a2c2a2bf483515c
louiscklaw <louiscklaw@gmail.com>
2021-10-09 19:23:28 +0800

    update,

---
+++ cypress-visual-regression-tryout/cypress/integration/pureudon/viewports/ipad-2.js
@@ -0,0 +1,13 @@
+describe("viewports", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("Visits the pureudon by ipad-2", () => {
+    cy.viewport("ipad-2");
+
+    cy.visit("https://pureudon.github.io");
+
+    cy.compareSnapshot("homepage", 0.0);
+  });
+});
