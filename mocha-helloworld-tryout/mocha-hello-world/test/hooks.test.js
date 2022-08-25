f67ec8b823a2b85b51f7764d1cf196f8218d1ee9
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-25 16:35:13 +0800

    update mocha mochaawesome,

--- mocha-helloworld-tryout/mocha-hello-world/test/hooks.test.js
+++ mocha-helloworld-tryout/mocha-hello-world/test/hooks.test.js
@@ -1,19 +1,19 @@
-describe("hooks", function () {
+describe('hooks', function () {
   before(function () {
     // runs once before the first test in this block
-  });
+  })
 
   after(function () {
     // runs once after the last test in this block
-  });
+  })
 
   beforeEach(function () {
     // runs before each test in this block
-  });
+  })
 
   afterEach(function () {
     // runs after each test in this block
-  });
+  })
 
   // test cases
-});
+})
--- mocha-helloworld-tryout/scripts/init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-npm install --also=dev
