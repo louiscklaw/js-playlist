3e225108278f020ccd4ed2729ce5dde55a7b2ea7
louiscklaw <logickee@gmail.com>
2023-01-04 03:44:21 +0800

    update,

---
+++ express-helloworld-tryout/express-handlebars-hotreload-tryout/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const { hotreload, engine } = require('express-handlebars-hotreload');
+
+if (process.env.NODE_ENV !== 'production') hotreload();
+
+const app = express();
+
+console.log(process.cwd() + '/views')
+
+app.engine('handlebars', engine({
+  hotreload: process.env.NODE_ENV !== 'production',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', process.cwd() + '/views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(4200);
