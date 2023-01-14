01b37cd288c22c290f9501ae290bafed4194e7c1
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-14 08:37:46 +0800

    update express-prisma-tryout,

--- prisma-tryout/express-prisma-tryout/index.ts
+++ prisma-tryout/express-prisma-tryout/index.ts
@@ -1,14 +1,88 @@
 import express, { Express, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const prisma = new PrismaClient()
 const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.get('/feed', async (req, res) => {
+
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  })
+
+  res.json(posts)
+
+})
+
+
+app.post('/post', async (req, res) => {
+
+  const { title, content, authorEmail } = req.body
+
+  const post = await prisma.post.create({
+
+    data: {
+
+      title,
+
+      content,
+
+      published: false,
+
+      author: { connect: { email: authorEmail } },
+
+    },
+
+  })
+
+  res.json(post)
+
+})
+
+
+app.put('/publish/:id', async (req, res) => {
+
+  const { id } = req.params
+
+  const post = await prisma.post.update({
+
+    where: { id },
+
+    data: { published: true },
+
+  })
+
+  res.json(post)
+
+})
+
+
+app.delete('/user/:id', async (req, res) => {
+
+  const { id } = req.params
+
+  const user = await prisma.user.delete({
+
+    where: {
+
+      id,
+
+    },
+
+  })
+
+  res.json(user)
+
+})
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ prisma-tryout/express-prisma-tryout/prisma/migrations/20230114002207_/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "likes" INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "likes" INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT "Profile_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
