a8ce8853abe37cb741d4022e41d38ac999528cd5
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-17 13:27:55 +0800

    update express-helloworld-tryout,

---
+++ express-helloworld-tryout/vhost-tryout/index.js
@@ -0,0 +1,19 @@
+var connect = require('connect')
+var vhost = require('vhost')
+var app = connect()
+
+app.use(
+  vhost('*.*.localhost', function handle(req, res, next) {
+    // for match of "foo.bar.example.com:8080" against "*.*.example.com":
+    console.dir({ '.host': req.vhost.host }) // => 'foo.bar.example.com:8080'
+    console.dir({ '.hostname': req.vhost.hostname }) // => 'foo.bar.example.com'
+    console.dir({ '.length': req.vhost.length }) // => 2
+    console.dir({ '[0': req.vhost[0] }) // => 'foo'
+    console.dir({ '[1': req.vhost[1] }) // => 'bar'
+
+    res.end('hello from mail!')
+  }),
+)
+
+console.log('Server running at http://foo.bar.localhost:3001/')
+app.listen(3001)
