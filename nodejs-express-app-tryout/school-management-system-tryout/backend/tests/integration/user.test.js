b33815e6b6fff87d6c29c7c04710c8cb32a5fe4c
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-05 02:55:34 +0800

    update test working,

--- nodejs-express-app-tryout/school-management-system-tryout/backend/tests/integration/user.test.js
+++ nodejs-express-app-tryout/school-management-system-tryout/backend/tests/integration/user.test.js
@@ -328,26 +328,26 @@ describe('User routes', () => {
       expect(res.body.results[1].id).toBe(userTwo._id.toHexString());
     });
 
-  //   test('should return the correct page if page and limit params are specified', async () => {
-  //     await insertUsers([userOne, userTwo, admin]);
+  test('should return the correct page if page and limit params are specified', async () => {
+    await insertUsers([userOne, userTwo, admin]);
 
-  //     const res = await request(app)
-  //       .get('/v1/users')
-  //       .set('Authorization', `Bearer ${adminAccessToken}`)
-  //       .query({ page: 2, limit: 2 })
-  //       .send()
-  //       .expect(httpStatus.OK);
+    const res = await request(app)
+      .get('/v1/users')
+      .set('Authorization', `Bearer ${adminAccessToken}`)
+      .query({ page: 2, limit: 2 })
+      .send()
+      .expect(httpStatus.OK);
 
-  //     expect(res.body).toEqual({
-  //       results: expect.any(Array),
-  //       page: 2,
-  //       limit: 2,
-  //       totalPages: 2,
-  //       totalResults: 3,
-  //     });
-  //     expect(res.body.results).toHaveLength(1);
-  //     expect(res.body.results[0].id).toBe(admin._id.toHexString());
-  //   });
+    expect(res.body).toEqual({
+      results: expect.any(Array),
+      page: 2,
+      limit: 2,
+      totalPages: 2,
+      totalResults: 3,
+    });
+    expect(res.body.results).toHaveLength(1);
+    expect(res.body.results[0].id).toBe(admin._id.toHexString());
+  });
   // });
 
   // describe('GET /v1/users/:userId', () => {
@@ -417,211 +417,211 @@ describe('User routes', () => {
   //   });
   // });
 
-  // describe('DELETE /v1/users/:userId', () => {
-  //   test('should return 204 if data is ok', async () => {
-  //     await insertUsers([userOne]);
+  describe('DELETE /v1/users/:userId', () => {
+    test('should return 204 if data is ok', async () => {
+      await insertUsers([userOne]);
 
-  //     await request(app)
-  //       .delete(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send()
-  //       .expect(httpStatus.NO_CONTENT);
+      await request(app)
+        .delete(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send()
+        .expect(httpStatus.NO_CONTENT);
 
-  //     const dbUser = await User.findById(userOne._id);
-  //     expect(dbUser).toBeNull();
-  //   });
+      const dbUser = await User.findById(userOne._id);
+      expect(dbUser).toBeNull();
+    });
 
-  //   test('should return 401 error if access token is missing', async () => {
-  //     await insertUsers([userOne]);
+    test('should return 401 error if access token is missing', async () => {
+      await insertUsers([userOne]);
 
-  //     await request(app).delete(`/v1/users/${userOne._id}`).send().expect(httpStatus.UNAUTHORIZED);
-  //   });
+      await request(app).delete(`/v1/users/${userOne._id}`).send().expect(httpStatus.UNAUTHORIZED);
+    });
 
-  //   test('should return 403 error if user is trying to delete another user', async () => {
-  //     await insertUsers([userOne, userTwo]);
+    test('should return 403 error if user is trying to delete another user', async () => {
+      await insertUsers([userOne, userTwo]);
 
-  //     await request(app)
-  //       .delete(`/v1/users/${userTwo._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send()
-  //       .expect(httpStatus.FORBIDDEN);
-  //   });
+      await request(app)
+        .delete(`/v1/users/${userTwo._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send()
+        .expect(httpStatus.FORBIDDEN);
+    });
 
-  //   test('should return 204 if admin is trying to delete another user', async () => {
-  //     await insertUsers([userOne, admin]);
+    test('should return 204 if admin is trying to delete another user', async () => {
+      await insertUsers([userOne, admin]);
 
-  //     await request(app)
-  //       .delete(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${adminAccessToken}`)
-  //       .send()
-  //       .expect(httpStatus.NO_CONTENT);
-  //   });
+      await request(app)
+        .delete(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${adminAccessToken}`)
+        .send()
+        .expect(httpStatus.NO_CONTENT);
+    });
 
-  //   test('should return 400 error if userId is not a valid mongo id', async () => {
-  //     await insertUsers([admin]);
+    test('should return 400 error if userId is not a valid mongo id', async () => {
+      await insertUsers([admin]);
 
-  //     await request(app)
-  //       .delete('/v1/users/invalidId')
-  //       .set('Authorization', `Bearer ${adminAccessToken}`)
-  //       .send()
-  //       .expect(httpStatus.BAD_REQUEST);
-  //   });
+      await request(app)
+        .delete('/v1/users/invalidId')
+        .set('Authorization', `Bearer ${adminAccessToken}`)
+        .send()
+        .expect(httpStatus.BAD_REQUEST);
+    });
 
-  //   test('should return 404 error if user already is not found', async () => {
-  //     await insertUsers([admin]);
+    test('should return 404 error if user already is not found', async () => {
+      await insertUsers([admin]);
 
-  //     await request(app)
-  //       .delete(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${adminAccessToken}`)
-  //       .send()
-  //       .expect(httpStatus.NOT_FOUND);
-  //   });
-  // });
+      await request(app)
+        .delete(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${adminAccessToken}`)
+        .send()
+        .expect(httpStatus.NOT_FOUND);
+    });
+  });
 
-  // describe('PATCH /v1/users/:userId', () => {
-  //   test('should return 200 and successfully update user if data is ok', async () => {
-  //     await insertUsers([userOne]);
-  //     const updateBody = {
-  //       name: faker.name.findName(),
-  //       email: faker.internet.email().toLowerCase(),
-  //       password: 'newPassword1',
-  //     };
+  describe('PATCH /v1/users/:userId', () => {
+    test('should return 200 and successfully update user if data is ok', async () => {
+      await insertUsers([userOne]);
+      const updateBody = {
+        name: faker.name.findName(),
+        email: faker.internet.email().toLowerCase(),
+        password: 'newPassword1',
+      };
 
-  //     const res = await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.OK);
+      const res = await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.OK);
 
-  //     expect(res.body).not.toHaveProperty('password');
-  //     expect(res.body).toEqual({
-  //       id: userOne._id.toHexString(),
-  //       name: updateBody.name,
-  //       email: updateBody.email,
-  //       role: 'user',
-  //       isEmailVerified: false,
-  //     });
+      expect(res.body).not.toHaveProperty('password');
+      expect(res.body).toEqual({
+        id: userOne._id.toHexString(),
+        name: updateBody.name,
+        email: updateBody.email,
+        role: 'user',
+        isEmailVerified: false,
+      });
 
-  //     const dbUser = await User.findById(userOne._id);
-  //     expect(dbUser).toBeDefined();
-  //     expect(dbUser.password).not.toBe(updateBody.password);
-  //     expect(dbUser).toMatchObject({ name: updateBody.name, email: updateBody.email, role: 'user' });
-  //   });
+      const dbUser = await User.findById(userOne._id);
+      expect(dbUser).toBeDefined();
+      expect(dbUser.password).not.toBe(updateBody.password);
+      expect(dbUser).toMatchObject({ name: updateBody.name, email: updateBody.email, role: 'user' });
+    });
 
-  //   test('should return 401 error if access token is missing', async () => {
-  //     await insertUsers([userOne]);
-  //     const updateBody = { name: faker.name.findName() };
+    test('should return 401 error if access token is missing', async () => {
+      await insertUsers([userOne]);
+      const updateBody = { name: faker.name.findName() };
 
-  //     await request(app).patch(`/v1/users/${userOne._id}`).send(updateBody).expect(httpStatus.UNAUTHORIZED);
-  //   });
+      await request(app).patch(`/v1/users/${userOne._id}`).send(updateBody).expect(httpStatus.UNAUTHORIZED);
+    });
 
-  //   test('should return 403 if user is updating another user', async () => {
-  //     await insertUsers([userOne, userTwo]);
-  //     const updateBody = { name: faker.name.findName() };
+    test('should return 403 if user is updating another user', async () => {
+      await insertUsers([userOne, userTwo]);
+      const updateBody = { name: faker.name.findName() };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userTwo._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.FORBIDDEN);
-  //   });
+      await request(app)
+        .patch(`/v1/users/${userTwo._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.FORBIDDEN);
+    });
 
-  //   test('should return 200 and successfully update user if admin is updating another user', async () => {
-  //     await insertUsers([userOne, admin]);
-  //     const updateBody = { name: faker.name.findName() };
+    test('should return 200 and successfully update user if admin is updating another user', async () => {
+      await insertUsers([userOne, admin]);
+      const updateBody = { name: faker.name.findName() };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${adminAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.OK);
-  //   });
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${adminAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.OK);
+    });
 
-  //   test('should return 404 if admin is updating another user that is not found', async () => {
-  //     await insertUsers([admin]);
-  //     const updateBody = { name: faker.name.findName() };
+    test('should return 404 if admin is updating another user that is not found', async () => {
+      await insertUsers([admin]);
+      const updateBody = { name: faker.name.findName() };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${adminAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.NOT_FOUND);
-  //   });
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${adminAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.NOT_FOUND);
+    });
 
-  //   test('should return 400 error if userId is not a valid mongo id', async () => {
-  //     await insertUsers([admin]);
-  //     const updateBody = { name: faker.name.findName() };
+    test('should return 400 error if userId is not a valid mongo id', async () => {
+      await insertUsers([admin]);
+      const updateBody = { name: faker.name.findName() };
 
-  //     await request(app)
-  //       .patch(`/v1/users/invalidId`)
-  //       .set('Authorization', `Bearer ${adminAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.BAD_REQUEST);
-  //   });
+      await request(app)
+        .patch(`/v1/users/invalidId`)
+        .set('Authorization', `Bearer ${adminAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.BAD_REQUEST);
+    });
 
-  //   test('should return 400 if email is invalid', async () => {
-  //     await insertUsers([userOne]);
-  //     const updateBody = { email: 'invalidEmail' };
+    test('should return 400 if email is invalid', async () => {
+      await insertUsers([userOne]);
+      const updateBody = { email: 'invalidEmail' };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.BAD_REQUEST);
-  //   });
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.BAD_REQUEST);
+    });
 
-  //   test('should return 400 if email is already taken', async () => {
-  //     await insertUsers([userOne, userTwo]);
-  //     const updateBody = { email: userTwo.email };
+    test('should return 400 if email is already taken', async () => {
+      await insertUsers([userOne, userTwo]);
+      const updateBody = { email: userTwo.email };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.BAD_REQUEST);
-  //   });
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.BAD_REQUEST);
+    });
 
-  //   test('should not return 400 if email is my email', async () => {
-  //     await insertUsers([userOne]);
-  //     const updateBody = { email: userOne.email };
+    test('should not return 400 if email is my email', async () => {
+      await insertUsers([userOne]);
+      const updateBody = { email: userOne.email };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.OK);
-  //   });
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.OK);
+    });
 
-  //   test('should return 400 if password length is less than 8 characters', async () => {
-  //     await insertUsers([userOne]);
-  //     const updateBody = { password: 'passwo1' };
+    test('should return 400 if password length is less than 8 characters', async () => {
+      await insertUsers([userOne]);
+      const updateBody = { password: 'passwo1' };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.BAD_REQUEST);
-  //   });
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.BAD_REQUEST);
+    });
 
-  //   test('should return 400 if password does not contain both letters and numbers', async () => {
-  //     await insertUsers([userOne]);
-  //     const updateBody = { password: 'password' };
+    test('should return 400 if password does not contain both letters and numbers', async () => {
+      await insertUsers([userOne]);
+      const updateBody = { password: 'password' };
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.BAD_REQUEST);
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.BAD_REQUEST);
 
-  //     updateBody.password = '11111111';
+      updateBody.password = '11111111';
 
-  //     await request(app)
-  //       .patch(`/v1/users/${userOne._id}`)
-  //       .set('Authorization', `Bearer ${userOneAccessToken}`)
-  //       .send(updateBody)
-  //       .expect(httpStatus.BAD_REQUEST);
-  //   });
-  // });
+      await request(app)
+        .patch(`/v1/users/${userOne._id}`)
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send(updateBody)
+        .expect(httpStatus.BAD_REQUEST);
+    });
+  });
 
 
   test('teacher api helloworld', async () => {
