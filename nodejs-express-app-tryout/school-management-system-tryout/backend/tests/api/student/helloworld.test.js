bef8d56a84ca6c509cdd41ac75f73854998958db
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-05 12:16:18 +0800

    update test,

--- nodejs-express-app-tryout/school-management-system-tryout/backend/tests/api/student/helloworld.test.js
+++ nodejs-express-app-tryout/school-management-system-tryout/backend/tests/api/student/helloworld.test.js
@@ -16,7 +16,7 @@ const { roleRights } = require('../../../src/config/roles');
 const { tokenTypes } = require('../../../src/config/tokens');
 
 const { userOne, admin, insertUsers } = require('../../fixtures/user.fixture');
-const { studentOne, insertStudents } = require('../../fixtures/student.fixture');
+const { studentOne, studentTwo, insertStudents } = require('../../fixtures/student.fixture');
 const { userOneAccessToken, adminAccessToken, studentOneAccessToken } = require('../../fixtures/token.fixture');
 
 setupTestDB();
@@ -86,8 +86,8 @@ describe('Auth routes', () => {
     });
   })
 
-
-  test('create student information', async () => {
+  // NOTE: createStudent
+  test('create new student', async () => {
     const res = await request(app)
       .post('/v1/students')
       .set('Authorization', `Bearer ${studentOneAccessToken}`)
@@ -128,7 +128,69 @@ describe('Auth routes', () => {
       phone: "",
       state: "",
     });
+  })
 
+  // NOTE: getStudents
+  test('get student information', async () => {
+    await insertStudents([studentOne, studentTwo]);
+
+    const res = await request(app)
+      .get('/v1/students')
+      .set('Authorization', `Bearer ${studentOneAccessToken}`)
+      .expect(httpStatus.OK);
+
+    expect(res.body).toEqual({
+      results: expect.any(Array),
+      page: 1,
+      limit: 10,
+      totalPages: 1,
+      totalResults: 2,
+    });
+
+    expect(res.body.results).toHaveLength(2);
+    expect(res.body.results[0]).toEqual({
+      id: studentOne._id.toHexString(),
+      name: studentOne.name,
+      email: studentOne.email,
+      role: studentOne.role,
+      isEmailVerified: studentOne.isEmailVerified,
+      address1: "",
+      address2: "",
+      country: "",
+      hasDiscount: false,
+      isVerified: false,
+      phone: "",
+      state: "",
+    });
+  })
+
+  // NOTE: updateStudentById
+  test('modify student by id', async () => {
+    await insertStudents([studentOne]);
+
+    const res = await request(app)
+      .patch(`/v1/students/${studentOne._id}`)
+      .set('Authorization', `Bearer ${studentOneAccessToken}`)
+      .send({ name: 'blablabla' })
+      .expect(httpStatus.OK);
+
+    const dbUser = await Student.findById(studentOne._id);
+    expect(dbUser).toBeDefined();
+
+    expect(dbUser.name).toMatch('blablabla');
+  })
+
+  // NOTE: deleteStudentById
+  test('delete student by id', async () => {
+    await insertStudents([studentOne]);
+
+    const res = await request(app)
+      .delete(`/v1/students/${studentOne._id}`)
+      .set('Authorization', `Bearer ${studentOneAccessToken}`)
+      .expect(httpStatus.NO_CONTENT);
+
+    const dbStudentOne = await Student.findById(studentOne._id);
+    expect(dbStudentOne).toBeNull();
   })
 
   test('student api helloworld', async () => {
