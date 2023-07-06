e8cb749d5fb7d31e3fc987e43668c79005b029a8
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-07 02:01:09 +0800

    update cms pages,

--- nodejs-express-app-tryout/school-management-system-tryout/backend/seed/student.js
+++ nodejs-express-app-tryout/school-management-system-tryout/backend/seed/student.js
@@ -20,7 +20,7 @@ const seedStudent = () =>
       // console.log('connected')
     })
     .then(() => {
-      const saving = Array(1)
+      const saving = Array(35)
         .fill(0)
         .map((_, idx) => {
           const newStudent = new Student({
