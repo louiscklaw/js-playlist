5c1d6f340abe7992cfa75d5e718f9bdd8d480942
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-06 22:29:40 +0800

    update,

--- nodejs-express-app-tryout/school-management-system-tryout/frontend/app/src/pages/404.js
+++ nodejs-express-app-tryout/school-management-system-tryout/frontend/app/src/pages/404.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, } from 'react';
 import NextLink from 'next/link';
 import Head from 'next/head';
 import {
@@ -10,8 +10,10 @@ import {
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { gtm } from '../lib/gtm';
+import { useTranslation } from 'react-i18next';
 
 const NotFound = () => {
+  const { t } = useTranslation();
   const theme = useTheme();
   const mobileDevice = useMediaQuery(theme.breakpoints.down('sm'));
 
@@ -22,7 +24,9 @@ const NotFound = () => {
   return (
     <>
       <Head>
-        <title>Error: Not Found | Material Kit Pro</title>
+        <title>
+          Error: Not Found | Material Kit Pro
+        </title>
       </Head>
       <Box
         component="main"
@@ -36,7 +40,10 @@ const NotFound = () => {
       >
         <Container maxWidth="lg">
           <Typography align="center" variant={mobileDevice ? 'h4' : 'h1'}>
-            404: The page you are looking for isn’t here
+            404:
+          </Typography>
+          <Typography align="center" variant={mobileDevice ? 'h4' : 'h1'}>
+            {t("The page you are looking for isn't here")}
           </Typography>
           <Typography
             align="center"
@@ -44,37 +51,24 @@ const NotFound = () => {
             sx={{ mt: 0.5 }}
             variant="subtitle2"
           >
-            You either tried some shady route or you came here by mistake.
-            Whichever it is, try using the navigation.
+            {t('You either tried some shady route or you came here by mistake. Whichever it is, try using the navigation.')}
           </Typography>
           <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              mt: 6,
-            }}
+            sx={{ display: 'flex', justifyContent: 'center', mt: 6, }}
           >
             <Box
               alt="Under development"
               component="img"
               src={`/static/error/error404_${theme.palette.mode}.svg`}
-              sx={{
-                height: 'auto',
-                maxWidth: '100%',
-                width: 400,
-              }}
+              sx={{ height: 'auto', maxWidth: '100%', width: 400, }}
             />
           </Box>
           <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              mt: 6,
-            }}
+            sx={{ display: 'flex', justifyContent: 'center', mt: 6, }}
           >
             <NextLink href="/dashboard" passHref>
               <Button component="a" variant="outlined">
-                Back to Dashboard
+                {t('Back to Dashboard')}
               </Button>
             </NextLink>
           </Box>
