5517adb7334df87d87570d332bd6fd8721f0b63f
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-15 02:53:47 +0800

    update helloworld-tryout

--- browserless-tryout/helloworld-tryout/src/screenshot-jobsdb.js
+++ browserless-tryout/helloworld-tryout/src/screenshot-jobsdb.js
@@ -8,12 +8,12 @@ const puppeteer = require('puppeteer-core');
   );
   const page = await browser.newPage();
 
-  await page.setViewport({ width: 600, height: 9999 });
+  await page.setViewport({ width: 600, height: 299 });
   await page.goto(
     'https://hk.jobsdb.com/hk/search-jobs/validation/1',
     { waitUntil: 'networkidle2' });
   // await page.waitForSelector('img[alt="Google"]');
-  await page.screenshot({ path: 'jobsdb.png', fullPage: true });
+  await page.screenshot({ path: `${__dirname}/screens/jobsdb.png`, fullPage: true });
 
   await browser.close();
 })();
--- browserless-tryout/helloworld-tryout/src/screenshot.js
+++ browserless-tryout/helloworld-tryout/src/screenshot.js
@@ -8,6 +8,6 @@ const puppeteer = require('puppeteer-core');
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto('https://www.google.com/', { waitUntil: 'networkidle2' });
   await page.waitForSelector('img[alt="Google"]');
-  await page.screenshot({ path: 'google.png', fullPage: true });
+  await page.screenshot({ path: `${__dirname}/screens/google.png`, fullPage: true });
   await browser.close();
 })();
