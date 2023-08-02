c960ef5f9296d168ebb4744fd2e0e7da2016c658
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-02 10:52:02 +0800

    update docker-poe-tryout,

---
+++ express-restify-mongoose-tryout/helloworld/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const restify = require('express-restify-mongoose');
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+mongoose.connect('mongodb://mongo:27017/database');
+
+restify.serve(
+  router,
+  mongoose.model(
+    'Customer',
+    new mongoose.Schema({
+      name: { type: String, required: true },
+      comment: { type: String },
+    }),
+  ),
+);
+
+app.use(router);
+
+var PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+});
