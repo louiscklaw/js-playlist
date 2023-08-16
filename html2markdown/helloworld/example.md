30702cbaaee0ec9c8e8b8654f2d09f779aa42f7c
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-16 18:27:16 +0800

    update html2markdown,

--- html2markdown/helloworld/example.md
+++ html2markdown/helloworld/example.md
@@ -1,5 +1,13 @@
 # hello h1
+
 ### hello h3
+
 ##### hello h5
 
 body p ?
+
+- point 1
+- point 2
+- point 3
+- point 4
+- point 5
--- html2markdown/helloworld/index.js
+++ html2markdown/helloworld/index.js
@@ -4,16 +4,34 @@
 const {parse} = require('node-html-parser')
 const fs = require('fs');
 
+
+function htmlToMarkdown(html_string){
+  try {
+    var output = html_string
+      .replace(/ +\<h1\>(.*?)\<\/h1\>/g,'# $1')
+      .replace(/.+\<h2\>(.*?)\<\/h2\>/g,'## $1')
+      .replace(/.+\<h3\>(.*?)\<\/h3\>/g,'### $1')
+      .replace(/.+\<h4\>(.*?)\<\/h4\>/g,'#### $1')
+      .replace(/.+\<h5\>(.*?)\<\/h5\>/g,'##### $1')
+      .replace(/.+\<p\>(.*?)\<\/p\>/g,'\n$1\n')
+      .replace(/.+\<ul\>((.|\n)*?)\<\/ul\>/g,'$1')
+      .replace(/.+\<li\> *(.*?) *\<\/li\>/g,'- $1')
+
+    output = output
+    .replace(/^ +$/gm,'')
+      .replace(/\n\n\n+/g,'\n\n')
+      .trim()
+
+    return output
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const doc = fs.readFileSync('example.html', {encoding: 'utf8'})
 const parsed_doc = parse(doc).querySelector('div').innerHTML
 
-const md = parsed_doc
-  .replace(/ +\<h1\>(.*?)\<\/h1\>/g,'# $1')
-  .replace(/.+\<h2\>(.*?)\<\/h2\>/g,'## $1')
-  .replace(/.+\<h3\>(.*?)\<\/h3\>/g,'### $1')
-  .replace(/.+\<h4\>(.*?)\<\/h4\>/g,'#### $1')
-  .replace(/.+\<h5\>(.*?)\<\/h5\>/g,'##### $1')
-  .replace(/.+\<p\>(.*?)\<\/p\>/g,'\n$1\n')
-  .trim();
+
+const md = htmlToMarkdown(parsed_doc)
 
 fs.writeFileSync('example.md', md, {encoding:'utf8'})
