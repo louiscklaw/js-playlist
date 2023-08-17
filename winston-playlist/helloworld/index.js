780932f37e1127d18c0bf5bb80f6a0fc61b2f3e1
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-18 01:40:24 +0800

    update,

--- winston-playlist/helloworld/index.js
+++ winston-playlist/helloworld/index.js
@@ -6,11 +6,11 @@ const hello_json = {hello:'json'}
 
 myLogger.error('hello error')
 
-myLogger.info('test message %s', 'my string');
-myLogger.info('test message %d', 123);
-myLogger.info('test message %s, %s', 'first', 'second', { number: 123 });
+// myLogger.info('test message %s', 'my string');
+// myLogger.info('test message %d', 123);
+// myLogger.info('test message %s, %s', 'first', 'second', { number: 123 });
 
-myLogger.info(JSON.stringify(hello_json, null, 2))
+// myLogger.info(JSON.stringify(hello_json, null, 2))
 
 // logger.info('hello', { message: 'world' });
 
--- winston-playlist/helloworld/utils/logger.js
+++ winston-playlist/helloworld/utils/logger.js
@@ -2,24 +2,30 @@ const winston = require('winston');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
 
+const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+
 const logger = winston.createLogger({
   level: 'info',
   format:  format.combine(
-    format.splat(),
-    format.simple()
+    format.timestamp(),
+    myFormat
   ),
 
   // defaultMeta: { service: 'user-service' },
   transports: [
+    new winston.transports.Console(),
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new winston.transports.Console({
+//     format: winston.format.simple(),
+//   }));
+// }
 
 module.exports = {myLogger: logger}
